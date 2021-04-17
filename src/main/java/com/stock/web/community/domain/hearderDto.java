d60c0fed76f29070289df60a881bfd1064e7b984
ddh963963 <ddh963963@naver.com>
2021-04-17 15:47:07 +0900

    회원가입 및 header js 수정

---
+++ src/main/java/com/stock/web/community/domain/hearderDto.java
@@ -0,0 +1,23 @@
+package com.stock.web.community.domain;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class hearderDto {
+	private String id;
+	private String name;
+	private String password;
+	private String birthday; 
+
+}
