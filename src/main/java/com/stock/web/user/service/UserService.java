20ee73affb157d9088d0ceb19f0c40bd702b2f03
ddh963963 <ddh963963@naver.com>
2022-04-24 18:59:02 +0900

    Feat : 로그인 기록 조회 추가

--- src/main/java/com/stock/web/user/service/UserService.java
+++ src/main/java/com/stock/web/user/service/UserService.java
@@ -1,13 +1,25 @@
 package com.stock.web.user.service;
 
+import com.stock.web.user.domain.LoginHistory;
 import com.stock.web.user.domain.UserDto;
 
+import java.util.List;
+
 public interface UserService {
-	
-	public void register(UserDto user);
-	public int deleteAll();
-	public Integer login(UserDto user);
-	public Integer idCheck(UserDto user);
-	public UserDto userInfo(UserDto user);
-	public void userEdit(UserDto user);
+
+    public void register(UserDto user);
+
+    public int deleteAll();
+
+    public Integer login(UserDto user);
+
+    public Integer idCheck(UserDto user);
+
+    public UserDto userInfo(UserDto user);
+
+    public void userEdit(UserDto user);
+
+    public void loginHistroy(LoginHistory log);
+
+    public List<LoginHistory> loginList(UserDto user);
 }
