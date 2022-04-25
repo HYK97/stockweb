a9932b64d4c96eb5c935fa0dcfdd80fbe4edca3a
ddh963963 <ddh963963@naver.com>
2022-04-25 13:06:04 +0900

    ReFactor : 필요없는 import 제거, 오타 수정 및 코드 정렬

--- src/main/java/com/stock/web/community/mapper/CommunityMapper.java
+++ src/main/java/com/stock/web/community/mapper/CommunityMapper.java
@@ -1,33 +1,44 @@
 package com.stock.web.community.mapper;
 
-import java.util.List;
-import java.util.Map;
-
-import org.apache.ibatis.annotations.Param;
-
 import com.stock.web.community.domain.Comments;
 import com.stock.web.community.domain.CommunityDto;
 import com.stock.web.community.domain.Stock;
-import com.stock.web.community.domain.imagesDto;
+import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+import java.util.Map;
 
 
 public interface CommunityMapper {
-	
-	public List<Stock> autocomplete();
-	public void insert(Map<String ,Object> bbsid);
-	public List<Comments> commentsList(@Param("fpage")int fpage,@Param("epage")int epage,@Param("bid")Long bid);
-	public List<CommunityDto> selectList(@Param("fpage")int fpage,@Param("epage")int epage,@Param("user")String user);
-	public List<CommunityDto> searchList(@Param("fpage")int fpage,@Param("epage")int epage,@Param("user")String user,@Param("search")String search);
-	public CommunityDto selectContent(@Param("bid")Long bid,@Param("user")String user);
-	public void likePush(Map<String ,Object> map);
-	public void insertImage(Map<String ,Object> map);
-	public void writeComment(Comments com);
-	public List<CommunityDto> myContentList(@Param("fpage")int fpage,@Param("epage")int epage,@Param("user")String user);
-	public List<CommunityDto> likeContent(@Param("fpage")int fpage,@Param("epage")int epage,@Param("user")String user);
-	public int deleteContent(@Param("ID")Long ID);
-	public int deleteComment(Comments com);
-	public int updateComment(Comments com);
-	public void updateContent(Map<String ,Object> update);
-	
+
+    public List<Stock> autocomplete();
+
+    public void insert(Map<String, Object> bbsid);
+
+    public List<Comments> commentsList(@Param("fpage") int fpage, @Param("epage") int epage, @Param("bid") Long bid);
+
+    public List<CommunityDto> selectList(@Param("fpage") int fpage, @Param("epage") int epage, @Param("user") String user);
+
+    public List<CommunityDto> searchList(@Param("fpage") int fpage, @Param("epage") int epage, @Param("user") String user, @Param("search") String search);
+
+    public CommunityDto selectContent(@Param("bid") Long bid, @Param("user") String user);
+
+    public void likePush(Map<String, Object> map);
+
+    public void insertImage(Map<String, Object> map);
+
+    public void writeComment(Comments com);
+
+    public List<CommunityDto> myContentList(@Param("fpage") int fpage, @Param("epage") int epage, @Param("user") String user);
+
+    public List<CommunityDto> likeContent(@Param("fpage") int fpage, @Param("epage") int epage, @Param("user") String user);
+
+    public int deleteContent(@Param("ID") Long ID);
+
+    public int deleteComment(Comments com);
+
+    public int updateComment(Comments com);
+
+    public void updateContent(Map<String, Object> update);
+
 }	
