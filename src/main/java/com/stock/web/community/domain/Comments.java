b963feaeb6e792c0c55336e2a60fccb84027a5f8
ddh963963 <ddh963963@naver.com>
2022-04-25 13:06:04 +0900

    ReFactor : 필요없는 import 제거, 오타 수정 및 코드 정렬

--- src/main/java/com/stock/web/community/domain/Comments.java
+++ src/main/java/com/stock/web/community/domain/Comments.java
@@ -1,9 +1,5 @@
 package com.stock.web.community.domain;
 
-import java.util.List;
-
-import com.stock.web.user.domain.UserDto;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,9 +10,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class Comments {
-	private Long comment_id;
-	private String USER_ID;
-	private String writedate;
-	private String comments;
-	private Long bbs_id;
+
+    private Long comment_id;
+
+    private String USER_ID;
+
+    private String writedate;
+
+    private String comments;
+
+    private Long bbs_id;
 }
